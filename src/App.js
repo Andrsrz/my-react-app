774110d19f84dcf507b441d16bed5d176f5e856a
Andres Ruiz <andrsruiz@gmail.com>
2020-11-05 17:00:24 -0600

    Try JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+function formatName(user){
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Andres',
+  lastName: 'Ruiz'
+};
+
+function getGreeting(user){
+  if(user)
+    return <h1>Hello, {formatName(user)}</h1>
+  else
+    return <h1>Hello, Stranger</h1>
+}
+
+const element = getGreeting(user);
+const elementWithoutName = getGreeting();
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (element);
 }
 
 export default App;
