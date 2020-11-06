862d8e1c203157c4ded471ff17bc5e10e44748aa
Andres Ruiz <andrsruiz@gmail.com>
2020-11-05 20:53:34 -0600

    Use main component in app

--- src/App.js
+++ src/App.js
@@ -1,38 +1,10 @@
-import Clock from './Clock';
-
-function formatName(user){
-  return user.firstName + ' ' + user.lastName;
-}
-
-const user = {
-  firstName: 'Andres',
-  lastName: 'Ruiz'
-};
-
-/* JSX */
-function getGreeting(user){
-  if(user)
-    return <h1>Hello, {formatName(user)}</h1>
-  else
-    return <h1>Hello, Stranger</h1>
-}
-
-const element = getGreeting(user);
-const elementWithoutName = getGreeting();
-
-/* Attributes with JSX */
-const url='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png';
-const alt='This is a pokemon';
-const image = <img src={url} alt={alt}></img>;
+import SearchPokemon from './SearchPokemon';
 
 function App() {
   return (
 	/* Childrens */
 	<div id='app'>
-	  <Clock />
-	  {elementWithoutName}
-	  {element}
-	  {image}
+	  <SearchPokemon />
 	</div>
   );
 }
